014f90ee8e5224e94a9927f9da980221f32c2a35
korupjus <korupjus@udel.edu>
2024-09-01 16:03:34 -0400

    completes HW0

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,15 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world!"); 
+        Debug.Log("Hello world!");
+        Debug.LogWarning("This is a warning.");
+        Debug.LogError("Yea its cooked");
+        //Author: Justin Korup
+        /*
+         *Will we only be using C# or will we use different CS languages?
+         *My game idea is a card game that has random rewards and levels that are randomly generated each time.
+         *I would like to learn about more about random number generators in games.
+        */
     }
 
     // Update is called once per frame
